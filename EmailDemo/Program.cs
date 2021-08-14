27de7c2b0bb30af46dfc6db1d007572b2592af9d
Filipe Silva <filipeqs@outlook.com>
2021-08-14 08:04:47 -0400

    first commit

---
+++ EmailDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using FluentEmail.Core;
+using FluentEmail.Razor;
+using FluentEmail.Smtp;
+
+namespace EmailDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var sender = new SmtpSender(() => new SmtpClient("localhost")
+            {
+                EnableSsl = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Port = 25 // Check on Papercut Smtp App
+                //DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory,
+                //PickupDirectoryLocation = @"C:\Demos"
+            });
+
+            StringBuilder template = new StringBuilder();
+            template.AppendLine("Dear @Model.FirstName,");
+            template.AppendLine("<p>Thanks for purchasing @Model.ProductName. We hope you enjoy it!</p>");
+            template.AppendLine("- The Filipe Silva Team");
+
+            Email.DefaultSender = sender;
+            Email.DefaultRenderer = new RazorRenderer();
+
+            var email = await Email
+                .From("filipe@email.com")
+                .To("test@test.com", "Sue")
+                .Subject("Thanks!")
+                .UsingTemplate(template.ToString(), new { FirstName = "Filipe", ProductName = "Bacon-Wrapped Bacon" })
+                //.Body("Thanks for buying our product!")
+                .SendAsync();
+        }
+    }
+}
